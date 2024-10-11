01bc7c30021a2734d96b32a73a917fd1942b2265
Pearson33 <146853701+Pearson33@users.noreply.github.com>
2024-10-11 11:40:33 +0200

    prueba

--- app/src/main/java/com/example/codelingo/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/codelingo/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Esta es la home");
+        mText.setValue("Esta es la home pruebinchi");
     }
 
     public LiveData<String> getText() {
